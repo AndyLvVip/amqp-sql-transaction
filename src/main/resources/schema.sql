ed09299b34a6d87cd05dca27ba81363a9952a75e
andy.lv <lvhs@ucacc.com>
2019-03-04 14:01:12 +0800

    add dev profile

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,8 @@
+drop database  amqp_sql_tx;
 create database amqp_sql_tx;
 use amqp_sql_tx;
-drop table amqp_sql_tx;
+drop table if exists amqp_sql_tx;
 create table amqp_sql_tx (
 id char(36) not null,
 primary key (id)
 ) engine = Innodb, charset = utf8mb4;
-
-show full columns from amqp_sql_tx;
